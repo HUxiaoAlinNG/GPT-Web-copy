9f3dc322262d5a641ec6615879d1454c5458b030
hilin <hilin>
2023-07-15 22:56:10 +0800

    feat: 调整错误信息

--- app/components/InputOnetimePassword.tsx
+++ app/components/InputOnetimePassword.tsx
@@ -2,7 +2,7 @@
  * @Author: hilin hilin
  * @Date: 2023-07-15 15:57:58
  * @LastEditors: hilin hilin
- * @LastEditTime: 2023-07-15 18:44:49
+ * @LastEditTime: 2023-07-15 22:50:51
  * @FilePath: /GPT-Web-copy/app/components/InputOnetimePassword.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -51,9 +51,9 @@ export function PasswordMessageModal(props: { onClose: () => void }) {
                 onClick={doSubmit}
               />
             </div>
-            <div style={{visibility: showErr ? "inherit" : "hidden"}} className={styles["err-text"]} >
-              {Locale.Password.ErrMsg}
-            </div>
+          </div>
+          <div style={{visibility: showErr ? "inherit" : "hidden"}} className={styles["err-text"]} >
+            {Locale.Password.ErrMsg}
           </div>
         </div>
         
