f3480b403b3dfb235e8920de33564117ae472be5
hilin <hilin>
2023-07-15 22:58:20 +0800

    feat: 密码

--- app/components/InputOnetimePassword.tsx
+++ app/components/InputOnetimePassword.tsx
@@ -2,7 +2,7 @@
  * @Author: hilin hilin
  * @Date: 2023-07-15 15:57:58
  * @LastEditors: hilin hilin
- * @LastEditTime: 2023-07-15 22:50:51
+ * @LastEditTime: 2023-07-15 22:58:07
  * @FilePath: /GPT-Web-copy/app/components/InputOnetimePassword.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,7 +23,7 @@ export function PasswordMessageModal(props: { onClose: () => void }) {
     if (password.trim() === "") return;
     const res = await doSubmitPassword(password)
     if (res.code === 0) {
-      accessStore.updateCode(res.data);
+      accessStore.updateCode(res.message);
       props.onClose();
     } else {
       setShowErr(true)
